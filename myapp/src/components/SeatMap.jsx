185ad2a16fc2faf77024ff13a4d73a22ec062e23
irozg <irozg@yahoo.com>
2021-06-28 21:21:59 +0300

    add stage2

--- myapp/src/components/SeatMap.jsx
+++ myapp/src/components/SeatMap.jsx
@@ -3,20 +3,20 @@ import Rect from "./Rect";
 
 //canvas mount
 export function Canvas(props) {
-  
+
     const { draw } = props
     const canvasRef = useRef(null)
 
     useEffect(() => {
-    
+
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        
+
         //Our draw come here
         draw(context)
-      }, [draw])
-    
-    return <canvas ref={canvasRef} className="map" width="700" height="400"/>
+    }, [draw])
+
+    return <canvas ref={canvasRef} className="map" width="700" height="400" />
 }
 
 
@@ -29,14 +29,14 @@ export default function SeatMap() {
         let nextDrawX = 0;
         let nextDrawY = 0;
         let rects = [];
-    
+
         for (let i = 1; i <= 10; i++) {
             ctx.fillStyle = "#000";
             ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
             nextDrawX += nextDrawX + 65;
             for (let z = 1; z <= 50; z++) {
-                // let seat = [z, i, "platia", false];
-                var r = new Rect(nextDrawX, nextDrawY, width, height, {i,z});
+                let type = "stage1"
+                var r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
                 r.drawRect(ctx);
                 rects.push(r);
                 nextDrawX = r.x + r.width + 2;
@@ -44,14 +44,29 @@ export default function SeatMap() {
             nextDrawY = r.y + r.height + 2;
             nextDrawX = 0;
         }
-      }
+
+        for (let i = 1; i <= 5; i++) {
+            ctx.fillStyle = "#000";
+            ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
+            nextDrawX += nextDrawX + 65;
+            for (let z = 1; z <= 50; z++) {
+                let type = "stage2"
+                var r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
+                r.drawRect(ctx);
+                rects.push(r);
+                nextDrawX = r.x + r.width + 2;
+            }
+            nextDrawY = r.y + r.height + 2;
+            nextDrawX = 0;
+        }
+    }
 
     return <Canvas draw={draw} />
 
- }
+}
 
 
-  
+
 
 
 
