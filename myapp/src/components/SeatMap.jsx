f9f0926ade1af343704a2c1ffc6bbfbd986e28b0
irozg <irozg@yahoo.com>
2021-06-29 01:28:33 +0300

    add seat component

--- myapp/src/components/SeatMap.jsx
+++ myapp/src/components/SeatMap.jsx
@@ -3,54 +3,54 @@ import Rect from "./Rect";
 
 
 export default function SeatMap() {
-       
-        const  rects = {
-                set current(rect){
-                    this.rectsArr.push(rect);
-                },
-                 rectsArr:[],
-                get latest() {
-                    return rects;
-                  }
-            }
 
-        const draw = (ctx) => {
-            ctx.font = "14px sans-serif";
-            const width = 10;
-            const height = 20;
-            let nextDrawX = 0;
-            let nextDrawY = 0;
-
-            for (let i = 1; i <= 10; i++) {
-                ctx.fillStyle = "#000";
-                ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
-                nextDrawX += nextDrawX + 65;
-                for (let z = 1; z <= 50; z++) {
-                    let type = "stage1"
-                    var r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
-                    rects.current = r;
-                    r.drawRect(ctx);
-                    nextDrawX = r.x + r.width + 2;
-                }
-                nextDrawY = r.y + r.height + 2;
-                nextDrawX = 0;
-            }
-
-            for (let i = 1; i <= 5; i++) {
-                ctx.fillStyle = "#000";
-                ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
-                nextDrawX += nextDrawX + 65;
-                for (let z = 1; z <= 50; z++) {
-                    let type = "stage2"
-                    r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
-                    r.drawRect(ctx);
-                    rects.current = r;
-                    nextDrawX = r.x + r.width + 2;
-                }
-                nextDrawY = r.y + r.height + 2;
-                nextDrawX = 0;
-            }
+    const rects = {
+        set current(rect) {
+            this.rectsArr.push(rect);
+        },
+        rectsArr: [],
+        get latest() {
+            return rects;
         }
+    }
+
+    const draw = (ctx) => {
+        ctx.font = "14px sans-serif";
+        const width = 10;
+        const height = 20;
+        let nextDrawX = 0;
+        let nextDrawY = 0;
+
+        for (let i = 1; i <= 10; i++) {
+            ctx.fillStyle = "#000";
+            ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
+            nextDrawX += nextDrawX + 65;
+            for (let z = 1; z <= 50; z++) {
+                let type = "stage1"
+                var r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
+                rects.current = r;
+                r.drawRect(ctx);
+                nextDrawX = r.x + r.width + 2;
+            }
+            nextDrawY = r.y + r.height + 2;
+            nextDrawX = 0;
+        }
+
+        for (let i = 1; i <= 5; i++) {
+            ctx.fillStyle = "#000";
+            ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
+            nextDrawX += nextDrawX + 65;
+            for (let z = 1; z <= 50; z++) {
+                let type = "stage2"
+                r = new Rect(nextDrawX, nextDrawY, width, height, { i, z, type });
+                r.drawRect(ctx);
+                rects.current = r;
+                nextDrawX = r.x + r.width + 2;
+            }
+            nextDrawY = r.y + r.height + 2;
+            nextDrawX = 0;
+        }
+    }
 
     return <Canvas draw={draw} rects={rects.rectsArr} />
 
@@ -84,66 +84,54 @@ export function Canvas(props) {
     return <canvas ref={canvasRef} onClick={handleClick} className="map" width="700" height="400" />
 }
 
+export function Seat(props) {
 
-// function getClickedSeat(x, y, rects) {
-//     rectClicked = rects.find((rect) => {
-//         return rect.hasClick(x, y);
-//     });
-//     if (rectClicked !== undefined) {
-//         return seat = rectClicked.seat;
-//     } else return seat = ["", "", "", ""];
-// }
-
-// export function CreateSeatMap(props) {
-
-// let ctx = document.querySelector("#canvas").getContext("2d");
-// ctx.font = "14px sans-serif";
-// const size = [10, 20];
-// let nextDrawX = 0;
-// let nextDrawY = 0;
-// let coordinates = [nextDrawX, nextDrawY];
-// let rects = [];
-
-
-// for (i = 1; i <= 10; i++) {
-//     ctx.fillStyle = "#000";
-//     ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
-//     nextDrawX += nextDrawX + 65;
-//     coordinates = [nextDrawX, nextDrawY];
-//     for (z = 1; z <= 50; z++) {
-//         let seat = [z, i, "platia", false];
-//         var r = new Rect(size, coordinates, seat);
-//         r.draw();
-//         rects.push(r);
-//         nextDrawX = r.x + r.width + 2;
-//         coordinates = [nextDrawX, nextDrawY];
-//     }
-//     nextDrawY = r.y + r.height + 2;
-//     nextDrawX = 0;
-//     coordinates = [nextDrawX, nextDrawY];
-// }
+    const [seat,setSeat] = useState({ stage: "", seat: "", line:"", ticket:"" })
+    const handleSelect = (e) => {
+        setSeat({ ...post, ticket: e.target.value })
+    }
+   
+    return (
+        <div>
+            <label htmlfor="ticket">Τοποθεσία:</label>
+            <input type="text" name="stage" id="stage" disabled size="7"  />
+            <label htmlfor="seat" >Θέση:</label>
+            <input type="text" name="seat" disabled size="5" />
+            <label htmlfor="seat" >Σειρά:</label>
+            <input type="text" name="line" id="lineSelection" disabled size="5" />
+            <label htmlfor="ticket" >Εισητήριο:</label>
+            <select id="ticket" name="ticket" onChange={handleSelect}>
+                <option value="regural">Κανονικό</option>
+                <option value="student">Φοιτιτικό</option>
+            </select >
+        </div>
+    );
+}
 
 
-// for (i = 1; i <= 5; i++) {
-//     ctx.fillStyle = "#000";
-//     ctx.fillText("σειρά: " + i, nextDrawX, nextDrawY + 15);
-//     nextDrawX += nextDrawX + 65;
-//     coordinates = [nextDrawX, nextDrawY];
-//     for (z = 1; z <= 50; z++) {
-//         let seat = [z, i, "eksostis", false];
-//         r = new Rect(size, coordinates, seat);
-//         r.draw();
-//         rects.push(r);
-//         nextDrawX = r.x + r.width + 2;
-//         coordinates = [nextDrawX, nextDrawY];
-//     }
-//     nextDrawY = r.y + r.height + 2;
-//     nextDrawX = 0;
-//     coordinates = [nextDrawX, nextDrawY];
-// }
-
-// return rects;
-// }
-
-
-
+// <div class="container">
+// <div class="row  g-3 justify-content-md-center">
+//     <div class="col-lg-3 col-12">
+//         <label for="ticket" class="form-label">Τοποθεσία:</label>
+//         <input type="text" name="stage" class="form-control p-2" id="stage" disabled
+//             size="7">
+//     </div>
+//     <div class="col-lg-3 col-12">
+//         <label for="seat" class="form-label">Θέση:</label>
+//         <input type="text" name="seat" class="form-control p-2" id="seatSelection"
+//             disabled size="5">
+//     </div>
+//     <div class="col-lg-3 col-12">
+//         <label for="seat" class="form-label">Σειρά:</label>
+//         <input type="text" name="line" class="form-control p-2" id="lineSelection"
+//             disabled size="5">
+//     </div>
+//     <div class="col-lg-3 col-12">
+//         <label for="ticket" class="form-label">Εισητήριο:</label>
+//         <select id="ticket" class="form-select p-2" name="ticket">
+//             <option value="regural">Κανονικό</option>
+//             <option value="student">Φοιτιτικό</option>
+//         </select>
+//     </div>
+// </div><!-- row -->
+// </div><!-- conteiner -->
